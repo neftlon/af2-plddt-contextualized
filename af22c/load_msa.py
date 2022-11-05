54f5e6f74e0a4d7a9a21bb35b89693458e4b3dec
Johannes Spies <me@jospies.de>
2022-11-05 22:02:38 +0100

    Experiment with Neff score

--- af22c/load_msa.py
+++ af22c/load_msa.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import io
+import json
+
 from Bio import SeqIO
-from typing import Union, Tuple, List
 from collections import namedtuple
 from dataclasses import dataclass
 import tarfile
 import logging
-import itertools
+import tqdm
 
 MsaMatchAttribs = namedtuple("MsaMatchAttribs", [
     "target_id",
@@ -27,8 +28,12 @@ class MsaMatch:
     attribs: MsaMatchAttribs
     gapped_seq: str
 
+    def __getitem__(self, item: int):
+        """Get a residue by index. (Including gaps from MSA.)"""
+        return self.gapped_seq[item]
 
-def extract_query_and_matches(a3m: io.TextIOBase) -> Tuple[str, str, List[MsaMatch]]:
+
+def extract_query_and_matches(a3m: io.TextIOBase) -> tuple[str, str, list[MsaMatch]]:
     # TODO(johannes): do we really want _this_ parameter? or should we accept filename strings as well?
 
     seqs = list(SeqIO.parse(a3m, "fasta"))
@@ -51,23 +56,71 @@ def extract_query_and_matches(a3m: io.TextIOBase) -> Tuple[str, str, List[MsaMat
     return query.id, query.seq, matches
 
 
+def hamming_distance(s1, s2) -> int:
+    """Return the Hamming distance between equal-length sequences.
+
+    Taken from: https://en.wikipedia.org/wiki/Hamming_distance"""
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length.")
+    return sum(el1 != el2 for el1, el2 in zip(s1, s2))
+
+
+def normalized_hamming_distance(s1, s2) -> float:
+    return hamming_distance(s1, s2) / len(s1)
+
+
+def get_depth(query, matches: list[MsaMatch], theta_id=0.2) -> int:
+    """
+    Get the MSA for each column.
+
+    Depth is calculated based on the N_eff score introduced in [1]. This score was also used in AlphaFold 2 for
+    assessing MSA quality.
+
+    [1] Tianqi Wu, Jie Hou, Badri Adhikari, Jianlin Cheng, Analysis of several key factors influencing deep
+    learning-based inter-residue contact prediction, Bioinformatics, Volume 36, Issue 4, 15 February 2020,
+    Pages 1091–1098, https://doi.org/10.1093/bioinformatics/btz679
+    """
+    num_matches = len(matches)
+    n_eff = 0
+    for s in range(num_matches):
+        inv_pi_s = 0.0
+        for t in range(num_matches):
+            s_seq = matches[s].gapped_seq[:len(query)]
+            t_seq = matches[t].gapped_seq[:len(query)]
+            dist = normalized_hamming_distance(s_seq, t_seq)
+            if dist < theta_id:
+                inv_pi_s += 1.0
+        pi_s = 1.0 / inv_pi_s
+        n_eff += pi_s
+    return n_eff
+
+
 def proteome_wide_analysis():
     proteome_msas_filename = "data/UP000001816_190650.tar.gz"
     with tarfile.open(proteome_msas_filename) as tar:
         logging.debug(f"opened {proteome_msas_filename}")
         names = tar.getnames()
         logging.debug(f"found around {len(names)} .a3m files containing MSAs")
-        for name in names:
+        depths = {}
+        # TODO: this loop looks like a bottleneck -- make it parallel?
+        for name in tqdm.tqdm(names):
             if name.endswith(".a3m"):
                 # process .a3m member
                 with tar.extractfile(name) as raw_a3m:
                     with io.TextIOWrapper(raw_a3m) as a3m:
                         query_id, query_seq, matches = extract_query_and_matches(a3m)
-                        # TODO: do something with matches
+                        prot_depths = get_depth(query_seq, matches)
+                        depths[query_id] = prot_depths
+
+        with open("data/UP000001816_190650_depths.json", "w") as f:
+            json.dump(depths, f)
 
 
 # experiments with MSA files
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
+    #proteome_wide_analysis()
     with open("data/Q9A7K5.a3m") as a3m:
         query_id, query_seq, matches = extract_query_and_matches(a3m)
+        depths = get_depth(query_seq, matches)
+        print(depths)
