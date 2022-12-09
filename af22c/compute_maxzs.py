e903fc47d3aea510984133d2500ff412dda04906
Simon Koch <simon.leo.koch@tum.de>
2022-12-09 14:16:26 +0100

    Refactor to work with new metric classes

--- af22c/compute_maxzs.py
+++ af22c/compute_maxzs.py
@@ -1,6 +1,5 @@
 import argparse
-from af22c.proteome import ProteomeMSAs, ProteomeMSASizes
-from af22c.score_max_z import calc_max_z
+from af22c.proteome import ProteomeMSAs, ProteomeMSASizes, ProteomeMaxZs
 import logging
 import math
 from pathlib import Path
@@ -10,7 +9,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("proteome_dir")
+    parser.add_argument("proteome_msas_dir")
     parser.add_argument("data_dir")
     parser.add_argument("msa_sizes_file")
     parser.add_argument("-n", "--max_n_sequences", default=math.inf, type=int)
@@ -20,8 +19,9 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dry_run", action="store_true")
     args = parser.parse_args()
 
-    proteome = ProteomeMSAs.from_directory(args.proteome_dir)
+    proteome = ProteomeMSAs.from_directory(args.proteome_msas_dir)
     msa_sizes = ProteomeMSASizes.from_file(args.msa_sizes_file)
+    maxzs = ProteomeMaxZs.from_msas(proteome, str(Path(args.data_dir) / "maxzs"))
 
     uniprot_ids = proteome.get_uniprot_ids()
     msa_sizes_ids = msa_sizes.get_uniprot_ids()
@@ -37,16 +37,14 @@ if __name__ == "__main__":
     )
     uniprot_ids &= ids_in_size
     logging.info(
-        f"computing Neffs for {len(uniprot_ids)} MSAs "
+        f"computing scores for {len(uniprot_ids)} MSAs "
         f"with {args.min_query_length} <= query length <= {args.max_query_length} "
         f"and {args.min_n_sequences} <= number of sequences <= {args.max_n_sequences} ..."
     )
 
     if not args.dry_run:
         for uniprot_id in uniprot_ids:
-            msa = proteome[uniprot_id]
-            scores = calc_max_z(msa)
-            with open(
-                Path(args.data_dir) / "maxzs" / f"{uniprot_id}.json", "w"
-            ) as outfile:
-                json.dump(scores, outfile)
+            try:
+                maxzs.compute_scores_by_id(uniprot_id, overwrite_scores=True)
+            except ValueError as e:
+                logging.error(f"Unable to compute maxzs scores for {uniprot_id}. \n {e}")
--- af22c/compute_neffs.py
+++ af22c/compute_neffs.py
@@ -1,42 +1,49 @@
 import argparse
-from af22c.proteome import Proteome
+from af22c.proteome import ProteomeMSAs, ProteomeMSASizes, ProteomeNeffs, ProteomeNeffsNaive
 import logging
 import math
+from pathlib import Path
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("proteome_dir")
+    parser.add_argument("proteome_msas_dir")
     parser.add_argument("data_dir")
+    parser.add_argument("msa_sizes_file")
     parser.add_argument("-n", "--max_n_sequences", default=math.inf, type=int)
     parser.add_argument("-l", "--max_query_length", default=math.inf, type=int)
     parser.add_argument("-m", "--min_n_sequences", default=0, type=int)
     parser.add_argument("-k", "--min_query_length", default=0, type=int)
     parser.add_argument("-d", "--dry_run", action="store_true")
+    # TODO add option for overwrite/keep already computed Neffs
     args = parser.parse_args()
 
-    proteome = Proteome.from_folder(args.proteome_dir, args.data_dir)
+    proteome = ProteomeMSAs.from_directory(args.proteome_msas_dir)
+    msa_sizes = ProteomeMSASizes.from_file(args.msa_sizes_file)
+    neffs = ProteomeNeffs.from_msas(proteome, str(Path(args.data_dir) / "neffs"))
+    neffs_naive = ProteomeNeffsNaive.from_msas(proteome, str(Path(args.data_dir) / "neffs_naive"))
 
-    msa_sizes = proteome.get_msa_sizes()
     uniprot_ids = proteome.get_uniprot_ids()
+    msa_sizes_ids = msa_sizes.get_uniprot_ids()
     logging.info(
-        f"found {len(uniprot_ids)} MSAs and sizes for {len(msa_sizes)} MSAs, "
+        f"found {len(uniprot_ids)} MSAs and sizes for {len(msa_sizes_ids)} MSAs, "
         f"selecting subset ..."
     )
-    subset_ids = proteome.get_uniprot_ids_in_size(
+    ids_in_size = msa_sizes.get_uniprot_ids_in_size(
         min_q_len=args.min_query_length,
         max_q_len=args.max_query_length,
         min_n_seq=args.min_n_sequences,
         max_n_seq=args.max_n_sequences,
     )
+    uniprot_ids &= ids_in_size
     logging.info(
-        f"computing Neffs for {len(subset_ids)} MSAs "
+        f"computing Neffs for {len(uniprot_ids)} MSAs "
         f"with {args.min_query_length} <= query length <= {args.max_query_length} "
         f"and {args.min_n_sequences} <= number of sequences <= {args.max_n_sequences} ..."
     )
 
     if not args.dry_run:
-        for uniprot_id in subset_ids:
-            proteome.compute_neff_by_id(uniprot_id)
-            proteome.compute_neff_naive_by_id(uniprot_id)
+        for uniprot_id in uniprot_ids:
+            neffs.compute_scores_by_id(uniprot_id)
+            neffs_naive.compute_scores_by_id(uniprot_id)
