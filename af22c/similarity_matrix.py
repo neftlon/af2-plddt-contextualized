7696ced7e00e0822375ab47151efb4848bcd7318
Simon Koch <simon.leo.koch@tum.de>
2022-12-09 16:31:06 +0100

    Add similarity matrix loading from txt files

--- af22c/similarity_matrix.py
+++ af22c/similarity_matrix.py
@@ -1,3 +1,18 @@
 import pandas as pd
+import numpy as np
 
-BLOSUM62 = pd.read_table("data/BLOSUM62.txt", sep="\\s+", comment="#")
+BLOSUM62_probs = pd.read_table("data/blosum62qij.txt", sep="\\s+", comment="#")
+BLOSUM62_probs.index = BLOSUM62_probs.columns
+BLOSUM62_probs = pd.DataFrame(np.tril(BLOSUM62_probs.values) + np.triu(BLOSUM62_probs.values.T, 1),
+                              columns=BLOSUM62_probs.columns,
+                              index=BLOSUM62_probs.index)
+PROT_SEQ_AAS = BLOSUM62_probs.index
+
+
+def get_normalized_similarity_matrix() -> np.ndarray:
+    # TODO return dataframe and refactor access in similarity_matrix.py
+    return BLOSUM62_probs.values
+
+
+def get_background_distribution(normalized_sim_matrix: np.ndarray) -> np.ndarray:
+    return np.sum(BLOSUM62_probs.values, axis=0)
