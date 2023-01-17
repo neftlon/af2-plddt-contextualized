7cf758625643de0199b95416bcb9f4eaba2e6500
Simon Koch <simon.leo.koch@tum.de>
2023-01-17 21:04:07 +0100

    Add option to generate random sample of specified size

--- scripts/generate_id_file.py
+++ scripts/generate_id_file.py
@@ -4,6 +4,7 @@ import logging
 import math
 import random
 import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -15,18 +16,25 @@ if __name__ == "__main__":
     parser.add_argument("-l", "--max_query_length", default=math.inf, type=int)
     parser.add_argument("-m", "--min_n_sequences", default=0, type=int)
     parser.add_argument("-k", "--min_query_length", default=0, type=int)
-    parser.add_argument("-b", "--big_representative_only", action="store_true", default=False)
+    parser.add_argument("-b", "--big_representative_only", action="store_true",
+                        default=False)
+    parser.add_argument("-s", "--random_sample_size", default=math.nan,
+                        type=int)
     args = parser.parse_args()
 
-    limits = args.min_query_length, args.max_query_length, args.min_n_sequences, args.max_n_sequences
+    limits = {
+        "min_q_len": args.min_query_length,
+        "max_q_len": args.max_query_length,
+        "min_n_seq": args.min_n_sequences,
+        "max_n_seq": args.max_n_sequences
+    }
     msa_sizes = ProteomeMSASizes.from_file(args.msa_sizes_file)
 
     if args.big_representative_only:
         # Assumption 100 away from max doesn't make much of a difference
-        args.min_n_sequences = max(0, args.max_n_sequences - 100)
-        args.min_query_length = max(0, args.max_query_length - 100)
-        uniprot_ids = msa_sizes.get_uniprot_ids_in_size(args.min_query_length, args.max_query_length,
-                                                        args.min_n_sequences, args.max_n_sequences)
+        limits["min_n_seq"] = max(0, args.max_n_sequences - 100)
+        limits["min_q_len"] = max(0, args.max_query_length - 100)
+        uniprot_ids = msa_sizes.get_uniprot_ids_in_size(**limits)
         if len(uniprot_ids) < 1:
             raise ValueError("No MSA in this size range!")
         representative = random.choice(tuple(uniprot_ids))
@@ -35,12 +43,27 @@ if __name__ == "__main__":
             f"{msa_sizes[representative][1]} query length and "
             f"{msa_sizes[representative][0]} sequences."
         )
-        pd.Series(representative).to_csv(args.result_file, index=False, header=False)
+        id_list = [representative]
+    elif not math.isnan(args.random_sample_size):
+        uniprot_ids = msa_sizes.get_uniprot_ids_in_size(**limits)
+        if len(uniprot_ids) < args.random_sample_size:
+            raise ValueError("Not enough MSA in this size range!")
+        uniprot_ids = random.sample(list(uniprot_ids), args.random_sample_size)
+        prot_sizes = np.array([msa_sizes[prot_id] for prot_id in uniprot_ids])
+        logging.info(f"Selected a random sample with the following statistics: "
+                     f"mean query length: {prot_sizes[:, 1].mean():.2f}, "
+                     f"min query length: {prot_sizes[:, 1].min()}, "
+                     f"max query length: {prot_sizes[:, 1].max()}, "
+                     f"mean number of sequences: {prot_sizes[:, 0].mean():.2f}, "
+                     f"min number of sequences: {prot_sizes[:, 0].min()}, "
+                     f"max number of sequences: {prot_sizes[:, 0].max()}")
+        id_list = pd.Series(uniprot_ids)
     else:
-        uniprot_ids = msa_sizes.get_uniprot_ids_in_size(args.min_query_length, args.max_query_length, args.min_n_sequences, args.max_n_sequences)
+        uniprot_ids = msa_sizes.get_uniprot_ids_in_size(**limits)
         logging.info(
-            f"Found {len(uniprot_ids)} MSAs with "
-            f"query length in [{args.min_query_length}, {args.max_query_length}] and "
-            f"number of sequences in [{args.min_n_sequences}, {args.max_n_sequences}]."
+            f"Found {len(uniprot_ids)} MSAs in size range {limits}."
         )
-        pd.Series(list(uniprot_ids)).to_csv(args.result_file, index=False, header=False)
+        id_list = pd.Series(list(uniprot_ids))
+
+    # Write to file
+    id_list.to_csv(args.result_file, index=False, header=False)
