8bcdf462a458f1bf6bd60c883ce4d845354636fc
Johannes Spies <me@jospies.de>
2023-01-24 13:49:24 +0100

    Test MSA parameter parsing

--- tests/test_proteome_msas.py
+++ tests/test_proteome_msas.py
@@ -4,6 +4,8 @@ import tempfile
 import tarfile
 from contextlib import contextmanager
 from io import BytesIO
+
+from af22c.load_msa import MsaMatchAttribs
 from af22c.proteome import ProteomeMSAs
 import pytest
 
@@ -24,6 +26,25 @@ MSA_FIXTURES = {
     """)
 }
 VALID_PROTEIN_IDS = {"F0042R", "B4R43L"}
+
+# NB: the scores in the following MSA headers are just hallucinated and are not reflected by the MSA itself
+MSA_WITH_PARAMS_FIXTURE = {
+    # hallucinated file content
+    "content": {
+        "alien/msas/111.a3m": textwrap.dedent("""\
+        >111
+        SEQ
+        >112\t42\t0.66\t0\t0\t3\t3\t0\t3\t3
+        EEQ
+        >113\t12\t0.33\t0\t1\t2\t2\t1\t2\t2
+        -E-
+        """),
+    },
+    "match_attribs": [
+        MsaMatchAttribs(42, 0.66, 0, 0, 3, 3, 0, 3, 3),
+        MsaMatchAttribs(12, 0.33, 0, 1, 2, 2, 1, 2, 2),
+    ]
+}
 
 
 def add_directory(tar: tarfile.TarFile, name: str):
@@ -47,10 +68,13 @@ def add_text_as_file(tar: tarfile.TarFile, path: str, text: str):
 
 
 @contextmanager
-def archived_sample_msa(compression=None):
+def archived_sample_msa(compression=None, fixture=None):
     """
     Create a temporary archive that contains the files specified in the `MSA_FIXTURES` constant.
     """
+    if fixture is None:
+        fixture = MSA_FIXTURES
+
     with tempfile.TemporaryDirectory() as temp_dirname:
         mode = "w"
         ext = ".tar"
@@ -60,7 +84,7 @@ def archived_sample_msa(compression=None):
         temp_tar_path = os.path.join(temp_dirname, "alien" + ext)
         with tarfile.open(temp_tar_path, mode=mode) as temp_tar:
             # add predefined fixture contents
-            for path, content in MSA_FIXTURES.items():
+            for path, content in fixture.items():
                 add_text_as_file(temp_tar, path, content)
 
         yield temp_tar_path
@@ -82,3 +106,13 @@ def test_load_sample_proteome_from_archive(compression):
         for msa in msas.get_msas():
             assert msa is not None
             assert msa.query_id in VALID_PROTEIN_IDS
+
+
+def test_sample_msa():
+    with archived_sample_msa(fixture=MSA_WITH_PARAMS_FIXTURE["content"]) as archive_path:
+        msas = ProteomeMSAs.from_archive(archive_path)
+        msas = list(msas.get_msas())
+        assert len(msas) == 1
+        msa = msas[0]
+        for match, expected_attribs in zip(msa.matches, MSA_WITH_PARAMS_FIXTURE["match_attribs"]):
+            assert match.attribs == expected_attribs
