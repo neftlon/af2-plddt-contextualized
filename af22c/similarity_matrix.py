c639b6515f0affdcb0a692cc9fabceff501fa98d
Johannes Spies <me@jospies.de>
2023-01-24 13:32:45 +0100

    Remove necessity of being in the right working directory for scores

--- af22c/similarity_matrix.py
+++ af22c/similarity_matrix.py
@@ -1,18 +1,27 @@
+import os
 import pandas as pd
 import numpy as np
 
-BLOSUM62_probs = pd.read_table("data/blosum62qij.txt", sep="\\s+", comment="#")
-BLOSUM62_probs.index = BLOSUM62_probs.columns
-BLOSUM62_probs = pd.DataFrame(np.tril(BLOSUM62_probs.values) + np.triu(BLOSUM62_probs.values.T, 1),
-                              columns=BLOSUM62_probs.columns,
-                              index=BLOSUM62_probs.index)
-PROT_SEQ_AAS = BLOSUM62_probs.index
+BLOSUM62_path = "data/blosum62qij.txt"
+BLOSUM62_probs = None
+PROT_SEQ_AAS = None
+if os.path.exists(BLOSUM62_path):  # don't panic yet if cwd is not right
+    BLOSUM62_probs = pd.read_table(BLOSUM62_path, sep="\\s+", comment="#")
+    BLOSUM62_probs.index = BLOSUM62_probs.columns
+    BLOSUM62_probs = pd.DataFrame(np.tril(BLOSUM62_probs.values) + np.triu(BLOSUM62_probs.values.T, 1),
+                                  columns=BLOSUM62_probs.columns,
+                                  index=BLOSUM62_probs.index)
+    PROT_SEQ_AAS = BLOSUM62_probs.index
 
 
 def get_normalized_similarity_matrix() -> np.ndarray:
+    if not BLOSUM62_probs:
+        raise Exception(f"unable to find BLOSUM62 matrix at {BLOSUM62_path}, cwd is {os.getcwd()}")
     # TODO return dataframe and refactor access in similarity_matrix.py
     return BLOSUM62_probs.values
 
 
 def get_background_distribution(normalized_sim_matrix: np.ndarray) -> np.ndarray:
+    if not BLOSUM62_probs:
+        raise Exception(f"unable to find BLOSUM62 matrix at {BLOSUM62_path}, cwd is {os.getcwd()}")
     return np.sum(BLOSUM62_probs.values, axis=0)
