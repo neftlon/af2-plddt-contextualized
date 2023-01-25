139e79721accc930f229163a624d3bbb25cf56d3
Johannes Spies <me@jospies.de>
2023-01-25 17:03:33 +0100

    Add code for plotting boxes of protein subregions to protein size distribution plots

--- af22c/plots/msa_sizes.py
+++ af22c/plots/msa_sizes.py
@@ -1,8 +1,15 @@
 import seaborn as sns
+import matplotlib.patches as patches
 from af22c.proteome import ProteomeMSASizes
+from af22c.plots import style
 
 
-def plot_msa_sizes(msa_sizes: ProteomeMSASizes, uniprot_ids: set[str] = None) -> sns.JointGrid:
+def plot_msa_sizes(
+    msa_sizes: ProteomeMSASizes,
+    uniprot_ids: set[str] = None,
+    log_scale: bool = True,
+    subsets: list[dict] = None,
+) -> sns.JointGrid:
     """
     Create a scatter plot showing a datapoint for a set of proteins depending on MSA meta information.
 
@@ -11,17 +18,56 @@ def plot_msa_sizes(msa_sizes: ProteomeMSASizes, uniprot_ids: set[str] = None) ->
     * number of AAs in protein (query length).
     These two parameters are projected on the coordinate axes of the plot.
     """
-    # prefiltering of proteins
-    msa_sizes_df = msa_sizes.get_msa_sizes()
-    if uniprot_ids:
-        if uniprot_ids - set(msa_sizes_df['uniprot_id']):
-            raise ValueError('Not all specified uniprot ids where found in the precomputed msa sizes file.')
-        msa_sizes_df = msa_sizes_df.loc[msa_sizes_df['uniprot_id'].isin(uniprot_ids)]
+    with style(start_idx=2) as c:
+        dataset_color = c()
 
-    # actual plotting
-    sns.set_style("whitegrid")
-    p = sns.jointplot(data=msa_sizes_df, x="query_length", y="sequence_count")
-    p.set_axis_labels(
-        "Number of Amino Acids in Query", "Number of Sequences in MSA"
-    )
-    return p
+        # prefiltering of proteins
+        msa_sizes_df = msa_sizes.get_msa_sizes()
+        if uniprot_ids:
+            if uniprot_ids - set(msa_sizes_df['uniprot_id']):
+                raise ValueError('Not all specified uniprot ids where found in the precomputed msa sizes file.')
+            msa_sizes_df = msa_sizes_df.loc[msa_sizes_df['uniprot_id'].isin(uniprot_ids)]
+
+        # actual plotting
+        #sns.set_style("whitegrid")
+        marginal_kws = {
+            "bins": 25,
+            "fill": True,
+            "color": dataset_color,
+        }
+        if log_scale:
+            marginal_kws["log_scale"] = True
+        p = sns.jointplot(
+            data=msa_sizes_df,
+            x="query_length",
+            y="sequence_count",
+            marginal_kws=marginal_kws,
+            facecolor=dataset_color,
+        )
+        p.set_axis_labels(
+            "Number of Amino Acids in Query", "Number of Sequences in MSA"
+        )
+        if log_scale:
+            p.ax_joint.set_xscale("log")
+            p.ax_joint.set_yscale("log")
+
+        if subsets:
+            rects, names = [], []
+            for idx, subset in enumerate(subsets):
+                name, (num_seqs_min, num_seqs_max), (query_length_min, query_length_max) = (
+                    subset["name"], subset["num_seqs_range"], subset["query_length_range"]
+                )
+                rect = patches.Rectangle(
+                    (query_length_min, num_seqs_min),
+                    query_length_max - query_length_min,
+                    num_seqs_max - num_seqs_min,
+                    linewidth=4,
+                    edgecolor=c(),
+                    facecolor="none",
+                )
+                p.ax_joint.add_patch(rect)
+                rects.append(rect)
+                names.append(name)
+            p.ax_joint.legend(rects, names)
+
+        return p
--- scripts/analyze_proteome_msa_sizes.py
+++ scripts/analyze_proteome_msa_sizes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
 
 from af22c.plots.msa_sizes import plot_msa_sizes
@@ -29,6 +30,15 @@ if __name__ == "__main__":
         help="Used for caching when `data_src` points to an archive or folder; also used for storing the output images "
              "when using the -p option.",
     )
+    parser.add_argument(
+        "-s",
+        "--subsets-file",
+        type=argparse.FileType("r"),
+        help="Specify ranges for protein subsets. The file must be a .json file with one array in it. The elements of "
+             "these arrays must be objects with the following fields: \"name\" for subset name, \"num_seqs_range\" to "
+             "specify an array with min/max entries of # of sequences, and \"query_length_range\" to specify an array "
+             "of min/max entries of query length",
+    )
     parser.add_argument("-c", "--compute_size", action="store_true")
     parser.add_argument("-p", "--plot_size", action="store_true")
     parser.add_argument("-d", "--duplicates", action="store_true")
@@ -49,18 +59,23 @@ if __name__ == "__main__":
     if args.compute_size:
         sizes.precompute_msa_sizes()
     if args.plot_size:
-        # create plot
-        jg = plot_msa_sizes(sizes)
+        subsets = None
+        if args.subsets_file:
+            subsets = json.load(args.subsets_file)
 
-        # save plot
-        data_dir = Path(args.data_dir)
-        name = "human"  # TODO: add support for other proteomes as well?
-        fig_dir = data_dir / "plots"
-        os.makedirs(fig_dir, exist_ok=True)
-        fig_path = fig_dir / f"{name}_msa_size_scatter.png"
-        jg.savefig(fig_path)
+        # create plots
+        for scale_ext, scale_opt in [("", False), ("_logscale", True)]:
+            jg = plot_msa_sizes(sizes, log_scale=scale_opt, subsets=subsets)
 
-        logging.info(f"saved figure to {fig_path}")
+            # save plot
+            data_dir = Path(args.data_dir)
+            name = "human"  # TODO: add support for other proteomes as well?
+            fig_dir = data_dir / "plots"
+            os.makedirs(fig_dir, exist_ok=True)
+            fig_path = fig_dir / f"{name}_msa_size_scatter{scale_ext}.png"
+            jg.savefig(fig_path)
+
+            logging.info(f"saved figure to {fig_path}")
     if args.duplicates:
         show_duplicates(sizes)
     # -----------------
