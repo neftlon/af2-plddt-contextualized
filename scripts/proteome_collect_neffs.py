b7cf9729e9e8680b1e5c6220356ed1f04032fd43
Johannes Spies <me@jospies.de>
2023-03-02 11:14:33 +0100

    Support archived input

--- scripts/proteome_collect_neffs.py
+++ scripts/proteome_collect_neffs.py
@@ -4,30 +4,63 @@
 round them (don't need so many (un)important decimals), and put them in a 
 single json file."""
 
-import os
-import sys
+import argparse
+from dataclasses import dataclass
 import json
+import os
+import subprocess as sp
+import sys
 from tqdm import tqdm
 
-SOURCE_DIR="./data/test"
-PROTEOME_NAME="UP000005640_9606"
-TARGET_FILE=f"./data/{PROTEOME_NAME}_neff_fast.json"
+# parse arguments
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("SOURCE",type=str,default="./data/test",help="source directory or archive (.tar or .tar.gz)")
+parser.add_argument("DESTFILE",type=argparse.FileType("w"),help="output .json file")
+args = parser.parse_args()
+
+# look which files should be processed
+srcmode = None
+prot_files = []
+if any(args.SOURCE.endswith(ext) for ext in [".tar",".tar.gz"]):
+  srcmode = "archive"
+  tarres = sp.run(f"tar -tf {args.SOURCE}",shell=True,capture_output=True)
+  if tarres.returncode == 0:
+    prot_files = tarres.stdout.decode().split()
+  else:
+    print("error: failed to get files from input archive",file=sys.stderr)
+    print("!!! cmd stdout !!!",tarres.stdout.decode(),sep="\n",file=sys.stderr)
+    print("!!! cmd stderr !!!",tarres.stderr.decode(),sep="\n",file=sys.stderr)
+    sys.exit(-1)
+elif os.path.isdir(args.SOURCE):
+  srcmode = "dir"
+  prot_files = os.listdir(args.SOURCE)
+if srcmode is None:
+  print("error: failed to determine input file type")
+  sys.exit(-1)
 
 # gather entries
-entries = {}
-for fn in tqdm(os.listdir(SOURCE_DIR), desc="gathering entries"):
+entries,xs = {},[]
+for fn in tqdm(prot_files, desc="gathering entries"):
   if fn.endswith(".json"):
-    path = os.path.join(SOURCE_DIR, fn)
-    with open(path) as f:
-      try:
-        xs = json.load(f)
-      except json.decoder.JSONDecodeError:
-        tqdm.write("failed to load %s: JSONDecodeError" % path)
-      else:
-        entries[fn[:-5]] = [round(x,2) for x in xs]
+    try:
+      if srcmode == "dir":
+        path = os.path.join(args.SOURCE, fn)
+        with open(path) as f:
+          xs = json.load(f)
+      elif srcmode == "archive":
+        z = "z" if args.SOURCE.endswith(".gz") else ""
+        tarres = sp.run(f"tar -{z}xOf {args.SOURCE} {fn}",shell=True,capture_output=True,check=True)
+        xs = json.loads(tarres.stdout.decode())
+    except json.decoder.JSONDecodeError:
+      tqdm.write("failed to load %s: JSONDecodeError" % fn)
+    except sp.CalledProcessError as tarerr:
+      tqdm.write(f"error: failed to get {fn} from input archive")
+      tqdm.write("!!! cmd stdout !!!\n" + tarerr.stdout.decode())
+      tqdm.write("!!! cmd stderr !!!\n" + tarerr.stderr.decode())
+    else:
+      entries[fn[:-5]] = [round(x,2) for x in xs]
 
 # write out file in compact json representation
-with open(TARGET_FILE,"w") as f:
-  json.dump(entries,f,separators=(',', ':'))
-print("dumped entries to %s" % TARGET_FILE)
+json.dump(entries,args.DESTFILE,separators=(',', ':'))
+print("dumped entries to %s" % args.DESTFILE.name)
 
