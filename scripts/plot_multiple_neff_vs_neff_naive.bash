6644a091672fac5b1069f0a7673d7876736ec057
Johannes Spies <13813209+neftlon@users.noreply.github.com>
2023-05-24 11:18:59 +0200

    Remove more scripts

--- scripts/plot_multiple_neff_vs_neff_naive.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# make Python imports work from current directory
-export PYTHONPATH="${PYTHONPATH}:`pwd`"
-
-./af22c/plots/neff_vs_neff_naive.py -p A8MX80
-./af22c/plots/neff_vs_neff_naive.py -p Q8IUR7
-./af22c/plots/neff_vs_neff_naive.py -p O43242
-./af22c/plots/neff_vs_neff_naive.py -p P06310
-./af22c/plots/neff_vs_neff_naive.py -p Q96QF7
