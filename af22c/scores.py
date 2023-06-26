fc114e0e0a06151c88dd22a8b5bd6081595d65bc
Johannes Spies <13813209+neftlon@users.noreply.github.com>
2023-06-26 13:09:54 +0200

    add Henikoff weighting to scores

--- af22c/scores.py
+++ af22c/scores.py
@@ -2,7 +2,7 @@ from af22c.msa import as_encmsa
 from itertools import chain
 import numpy as _ # prevent "Error: mkl-service + Intel(R) MKL: MKL_THREADING_LAYER=INTEL is incompatible with libgomp.so.1 library"
 import torch
-from tqdm import tqdm
+from tqdm import tqdm, trange
 
 def pwseq(msa, device=None, batch_size=2**12, verbose=False, **kwargs):
   """return pairwise sequence identity calculated with pytorch"""
@@ -45,7 +45,50 @@ def pwseq(msa, device=None, batch_size=2**12, verbose=False, **kwargs):
       bpwseq[batch_pairs[:,1],batch_pairs[:,0]] = batch_pwseq
     
     return bpwseq
-  
+
+def henikoff_seq_weights(msa, verbose=0):
+  """
+  Calculate "correct position-based sequence weight" by method from
+  Henikoff and Henikoff 1994.
+  """
+  with as_encmsa(msa):
+    # on-demand verbose range
+    vrange = lambda desc, *params: (
+      trange(*params, desc=desc)
+      if verbose else range(*params)
+    )
+    
+    # count number of unique residues in each column, hopefully this can
+    # be done more efficient
+    r = []
+    for j in vrange("count unique residues", msa.data.shape[1]):
+      _, cnts = msa.data[:,j].unique(return_counts=True)
+      r += [len(cnts)]
+    r = torch.tensor(r,dtype=torch.long)
+    
+    # count number of sequences containing a specific amino acid, this
+    # also looks like it could be sped up
+    s = torch.zeros(msa.data.shape[1],len(msa.vocab),dtype=torch.long)
+    for j in vrange("count seqs with AA", msa.data.shape[1]):
+      for i in range(msa.data.shape[0]):
+        s[j, msa.data[i,j].item()] += 1
+        
+    # calculate position specific sequence weights
+    w = torch.zeros_like(msa.data, dtype=torch.float32)
+    for j in vrange("calc weights", msa.data.shape[0]): # sequence j
+      for i in range(msa.data.shape[1]): # position i
+        a = msa.data[j,i]
+        w[j,i] = 1. / (r[i] * s[i, a.item()])
+    
+    # normalize sequence weights on the way out
+    return w.mean(dim=1)
+
+def identity_seq_weights(msa):
+  """
+  Calculate identity based sequence wjjjeights.
+  """
+  return 1 / torch.sum(pwseq(msa, verbose=1) >= .8, dim=0)
+
 def gapcount(msa, weights=None, nongap=False, **kwargs):
   """
   calculate number of gaps in each msa column. returns a vector of length query_len.
